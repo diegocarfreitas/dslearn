ba75f3b622da83dfa081f77183eb0ad45ad727f7
Diego Carvalho de Freitas <diegocar.freitas@hotmail.com>
2021-10-20 19:36:23 -0300

    Busca serviço por nível de acesso

--- backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/UserService.java
@@ -23,8 +23,12 @@ public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private AuthService authService;
+
     @Transactional(readOnly = true)
     public UserDTO findById(Long id) {
+        authService.validateSelfOrAdmin(id);
         Optional<User> optional = repository.findById(id);
         User entity = optional.orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado"));
         return new UserDTO(entity);
